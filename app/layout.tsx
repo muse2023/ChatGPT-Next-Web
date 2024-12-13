fd488dac6b0ba696b2390023f3ff06822e5f2553
muse2023 <pandasuperfans@gmail.com>
2024-12-14 03:47:16 +0800

    修改 title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "HappyChat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "HappyChat",
     statusBarStyle: "default",
   },
 };
